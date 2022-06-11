3fe430999877da9f854bf1433d20f1765e78b8db
nburchiel <nathan.burchiel@gmail.com>
2022-06-10 20:27:52 -0400

    logic added to calculator to require valid inputs from user

--- src/com/MortgageCalculator/Main.java
+++ src/com/MortgageCalculator/Main.java
@@ -8,20 +8,43 @@ public class Main {
         final byte MONTHS_PER_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
         Scanner scanner = new Scanner(System.in);
         //Principal(P)
-        System.out.print("Principal Balance: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal Balance ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000)
+                break;
+            System.out.println("Please enter a value between $1,000 and $1,000,000");
+        }
+
 
         //Annual Interest Rate(r): Entered number needs to be divided by 100 then 12 for monthly interest
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_PER_YEAR;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >= 0 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_PER_YEAR;
+                break;
+            }
+            System.out.println("Please enter a value between 0 and 30");
+
+        }
 
         //Loan Period Length in Years(n): number needs to be multiplied by 12
-        System.out.print("Loan Period (Years): ");
-        short years = scanner.nextShort();
-        int numberOfPayments = years * MONTHS_PER_YEAR;
+        while (true) {
+            System.out.print("Loan Period (Years): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * MONTHS_PER_YEAR;
+                break;
+            }
+            System.out.println("Enter a number between 1 and 30");
+        }
 
         //                                      r(1+r)^n
         //Monthly Mortgage Calculation:  M = --------------
